be50bce41f2be941d7bb0bee2c2adf4db774b34f
Marcos Gago <mgagog@alumnos.nebrija.es>
2023-10-27 16:51:38 +0200

    ejercicio

---
+++ types.ts
@@ -0,0 +1,14 @@
+export type Character = {
+    id: number,
+    nombre: string,
+    raza: string,
+    descripción: string;
+    habilidades: string;
+  }
+
+  export type Person = {
+    name: string,
+    dni: string,
+    age: number,
+    id: string
+  };
